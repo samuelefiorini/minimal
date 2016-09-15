c1960f4a7678334455eeee5784cb28ba2815af47
Samuele Fiorini <samuele.fiorini@gmail.com>
2016-09-15 15:29:02 +0200

    mini_test started

---
+++ scripts/mini_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Minimal testing script.
+
+This script, once minimal is trained on an appropriate training set, evaluates
+the error on a test set.
+"""
+
+######################################################################
+# Copyright (C) 2016 Samuele Fiorini, Annalisa Barla
+#
+# FreeBSD License
+######################################################################
