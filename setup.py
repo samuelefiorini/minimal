cf45beecad83b55ec9b52c798de949349095b1ad
Samuele Fiorini <samuele.fiorini@gmail.com>
2016-09-15 15:24:44 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+"""minimal setup script."""
+
+from distutils.core import setup
+
+# Package Version
+from minimal import __version__ as version
+
+setup(
+    name='minimal',
+    version=version,
+
+    description=('MatrIx regularizatioN In MAchine Learning'),
+    long_description=open('README.md').read(),
+    author='Samuele Fiorini, Annalisa Barla',
+    author_email='samuele.fiorini@dibris.unige.it, annalisa.barla@unige.it',
+    maintainer='Samuele Fiorini',
+    maintainer_email='samuele.fiorini@dibris.unige.it',
+    url='https://github.com/samuelefiorini/minimal',
+    # download_url='https://github.com/slipguru/adenine/tarball/'+version,
+    download_url='https://github.com/samuelefiorini/minimal/archive/master.zip',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS'
+    ],
+    license='FreeBSD',
+
+    packages=['minimal'],
+    requires=['numpy (>=1.10.1)',
+              'scipy (>=0.16.1)',
+              'sklearn (>=0.17)',
+              'matplotlib (>=1.5.1)',
+              'seaborn (>=0.7.0)'],
+    scripts=['scripts/mini_train.py', 'scripts/mini_test.py'],
+)
