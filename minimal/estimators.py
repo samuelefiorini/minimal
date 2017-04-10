28ea748d7aeedda6f8ed1e8e659a823535b4fd7e
Samuele Fiorini <samuele.fiorini@gmail.com>
2017-04-10 14:29:53 +0200

    added NNMRegressor

--- minimal/estimators.py
+++ minimal/estimators.py
@@ -312,3 +312,130 @@ class GroupLassoClassifier(LinearClassifierMixin, LinearModel):
     @property
     def classes_(self):
         return self._label_binarizer.classes_
+
+    class NNMRegressor(LinearModel, RegressorMixin):
+        """Multi-task linear regression with Nuclear-norm as regularizer.
+
+         Minimizes the objective function::
+
+                (1 / n_samples) * L(Y, XW) + alpha * ||W||_*
+
+        where ::
+
+                L(y, XW)
+
+        is a square-loss function and ::
+
+                ||W||_* = trace(sqrt(W^T W))
+
+        is the Nuclear (trace)-norm penalty.
+
+        Parameters
+        ----------
+        alpha : float
+            regularization parameter
+        algorithm : string
+            the selected minimization algorithm in {'ista', 'fista'}.
+        tol : float
+            stopping rule tolerance. Default is 1e-5.
+        max_iter : int
+            maximum number of iterations. Default is 1e4.
+        copy_X : boolean, optional, default True
+            If ``True``, X will be copied; else, it may be overwritten.
+        normalize : boolean, optional, default False
+            If ``True``, the regressors X will be normalized before regression.
+            This parameter is ignored when ``fit_intercept`` is set to ``False``.
+            When the regressors are normalized, note that this makes the
+            hyperparameters learnt more robust and almost independent of the number
+            of samples. The same property is not valid for standardized data.
+            However, if you wish to standardize, please use
+            :class:`sklearn.preprocessing.StandardScaler` before calling
+            ``fit`` on an estimator with ``normalize=False``.
+        return_iter : bool
+            return the number of iterations before convergence
+        """
+        def __init__(self, alpha=1.0, algorithm='FISTA', fit_intercept=True,
+                     tol=1e-5, max_iter=10000, copy_X=True, normalize=False,
+                     return_iter=False):
+            self.alpha = alpha
+            self.loss = 'square'
+            self.algorithm = algorithm
+            self.fit_intercept = fit_intercept
+            self.tol = tol
+            self.max_iter = max_iter
+            self.return_iter = return_iter
+            self.copy_X = copy_X
+            self.normalize = normalize
+            self.class_weight = None  # TODO support class weight in future
+
+        def fit(self, X, y, check_input=True):
+            """Fit model with proximal gradient method.
+
+             Parameters
+            -----------
+            X : ndarray, (n_samples, n_features)
+                Data
+            y : ndarray, shape (n_samples,)
+                Target
+            check_input : bool
+                perform input check  (default = True)
+            """
+            if self.alpha == 0:
+                warnings.warn("With alpha=0, this algorithm does not converge "
+                              "well. You are advised to use the LinearRegression "
+                              "estimator from scikit-learn", stacklevel=2)
+
+            # We expect X and y to be float64 or float32 Fortran ordered arrays
+            # when bypassing checks
+            if check_input:
+                X, y = check_X_y(X, y, accept_sparse=False,
+                                 order='C', dtype=[np.float64, np.float32],
+                                 copy=self.copy_X and self.fit_intercept,
+                                 multi_output=False, y_numeric=True)
+                y = check_array(y, order='C', copy=False, dtype=X.dtype.type,
+                                ensure_2d=False)
+
+            X, y, X_offset, y_offset, X_scale, precompute, Xy = \
+                _pre_fit(X, y, None, False, self.normalize,
+                         self.fit_intercept, copy=False)
+
+            if y.ndim == 1:
+                y = y[:, np.newaxis]
+            if Xy is not None and Xy.ndim == 1:
+                Xy = Xy[:, np.newaxis]
+
+            n_samples, n_features = X.shape
+            # n_targets = y.shape[1]
+
+            # Define group-lasso minimizer
+            args = {'loss': self.loss,
+                    'penalty': 'trace',
+                    'tau': self.alpha,
+                    'tol': self.tol,
+                    'max_iter': self.max_iter
+                    }
+
+            if self.algorithm.lower() == 'fista':
+                from minimal.optimization import FISTA
+                minimizer = partial(FISTA, **args)
+            elif self.algorithm.lower() == 'ista':
+                from minimal.optimization import ISTA
+                minimizer = partial(ISTA, **args)
+            else:
+                raise NotImplementedError('algorithm must be '
+                                          'in {}.'.format(__algorithms__))
+
+            # Run the optimization algorithm
+            if self.return_iter:
+                self.coef_, _, self.n_iter = minimizer(data=X, labels=y)
+            else:
+                self.coef_, _ = minimizer(data=X, labels=y)
+            self.coef_ = self.coef_.ravel()
+
+            # Set intercept
+            self._set_intercept(X_offset, y_offset, X_scale)
+
+            # workaround since _set_intercept will cast self.coef_ into X.dtype
+            self.coef_ = np.asarray(self.coef_, dtype=X.dtype)
+
+            return self
